9f1edaf49daae69161c6fd409298560a9adf8405
Sweetymeow <honeykatie@qq.com>
2018-08-24 13:59:42 -0700

    Loader Lib link

--- src/comps/CSSLoader.jsx
+++ src/comps/CSSLoader.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import "../styles/Loader.css";
 
+// https://loading.io/css/
 class CSSLoader extends React.Component {
   render () {
     <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
